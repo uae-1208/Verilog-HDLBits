ba22d0879831bfdabb21727196619e594f3d9f89
uae-1208 <78890463+uae-1208@users.noreply.github.com>
2023-06-20 23:20:34 +0800

    Add files via upload

---
+++ 2.Verilog_Language/2.3Procedures/2.4.7.Prority encoder with casez.v
@@ -0,0 +1,16 @@
+// synthesis verilog_input_version verilog_2001
+module top_module (
+    input [3:0] in,
+    output reg [1:0] pos  );
+
+    always @* begin
+        casex(in)
+            4'b0000: pos = 0;
+            4'b???1: pos = 0;
+            4'b??10: pos = 1;
+            4'b?100: pos = 2;
+            4'b1000: pos =3;
+        endcase
+    end
+    
+endmodule
